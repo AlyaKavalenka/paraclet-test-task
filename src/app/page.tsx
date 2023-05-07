7ef279b1565127dec0073b9191ed6220457d4604
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-07 14:15:19 +0300

    feat: add prettier, eslint, stylelint-airbnb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -15,11 +15,11 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
-              className={styles.vercelLogo}
+              className="vercel-logo"
               width={100}
               height={24}
               priority
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
