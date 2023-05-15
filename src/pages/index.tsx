8fb36722d37b17e2097331cb66b23aae22ff7c2d
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-15 21:15:47 +0300

    feat: add search component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header/Header";
 import homeStyles from "./index.module.scss";
 import Filter from "@/components/Filter/Filter";
+import Search from "@/components/Search/Search";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
         <Header activePage="SearchPage" />
         <section className={homeStyles.home__main}>
           <Filter />
+          <section className={homeStyles.searchVacancy}>
+            <Search />
+          </section>
         </section>
       </aside>
     </main>
