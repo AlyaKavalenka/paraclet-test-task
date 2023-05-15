9226430e951983605392e5cde70fde8db4620804
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-15 20:59:57 +0300

    feat: add filter component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Header from "@/components/Header/Header";
+import homeStyles from "./index.module.scss";
+import Filter from "@/components/Filter/Filter";
 
 export default function Home() {
   return (
     <main>
-      <Header activePage="SearchPage" />
-      <p>Heloo World</p>
+      <aside className={homeStyles.home}>
+        <Header activePage="SearchPage" />
+        <section className={homeStyles.home__main}>
+          <Filter />
+        </section>
+      </aside>
     </main>
   );
 }
