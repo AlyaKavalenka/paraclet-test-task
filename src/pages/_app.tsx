2dfe9183c93bf67162c5513cb6ca473687d2f234
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-15 21:11:14 +0300

    feat: add Mantine lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "../index.scss";
 import type { AppProps /* , AppContext */ } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        fontFamily: "Inter, sans-serif",
+        colorScheme: "light",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
