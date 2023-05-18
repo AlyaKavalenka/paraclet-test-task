93d83bc2cc3c439ca2576e625b2e1f1c2ef21b48
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-18 20:26:09 +0300

    feat: add basic layout for search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,3 +1,12 @@
+import { Button, TextInput } from "@mantine/core";
+import SearchIcon from "@/assets/svg/searchIcon";
+
 export default function Search() {
-  return <div>Search Block</div>;
+  return (
+    <TextInput
+      placeholder="Введите название вакансии"
+      icon={<SearchIcon />}
+      rightSection={<Button>Поиск</Button>}
+    />
+  );
 }
