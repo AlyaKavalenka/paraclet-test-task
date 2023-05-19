0fdddce0b233523606de38aee4803ea2d38a4c48
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-19 15:33:19 +0300

    feat: style search

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,16 @@
 import { Button, TextInput } from "@mantine/core";
 import SearchIcon from "@/assets/svg/searchIcon";
+import searchStyle from "./search.module.scss";
 
 export default function Search() {
   return (
-    <TextInput
-      placeholder="Введите название вакансии"
-      icon={<SearchIcon />}
-      rightSection={<Button>Поиск</Button>}
-    />
+    <div className={searchStyle.search}>
+      <TextInput
+        placeholder="Введите название вакансии"
+        icon={<SearchIcon />}
+        className={searchStyle.search__input}
+      />
+      <Button className={searchStyle.search__btn}>Поиск</Button>
+    </div>
   );
 }
