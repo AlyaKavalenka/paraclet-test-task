e902f0c19b868f9e3a75028e195fab45893cf442
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-22 17:52:36 +0300

    feat: add new slicer

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,11 +7,13 @@ import {
 import CataloguesSlice from "./Slicers/CataloguesSlice";
 import VacanciesSlice from "./Slicers/VacanciesSlice";
 import ClickedVacancySlice from "./Slicers/ClickedVacancySlice";
+import FavoritesSlice from "./Slicers/FavoritesSlice";
 
 const rootReducer = combineReducers({
   cataloguesSlice: CataloguesSlice,
   vacanciesSlice: VacanciesSlice,
   clickedVacancy: ClickedVacancySlice,
+  favorites: FavoritesSlice,
 });
 
 export const store = configureStore({
