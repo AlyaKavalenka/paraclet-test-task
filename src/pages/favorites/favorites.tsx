01a29cabdd4e207fd54ba7d29cf4d0a3f76a9dac
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-22 19:05:07 +0300

    feat: add & style page with no favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -1,17 +1,21 @@
 import Header from "@/components/Header/Header";
 import favoritesStyles from "./favorites.module.scss";
+import { useAppSelector } from "@/store/hooks";
+import NoFavorites from "@/components/NoFavorites/NoFavorites";
 
 export default function FavoritesPage() {
+  const faveVacancies = useAppSelector((state) => state.favorites.value);
+
   return (
     <main>
       <aside className={favoritesStyles.favorites}>
         <Header activePage="MarkedPage" />
         <section className={favoritesStyles.favorites__main}>
           <div className={favoritesStyles.favorites__mainWrapper}>
-            <div>Favorites page</div>
+            {faveVacancies.length ? <div>Favorites page</div> : <NoFavorites />}
           </div>
         </section>
-        <section>pagination block</section>
+        {faveVacancies.length ? <section>pagination block</section> : ""}
       </aside>
     </main>
   );
