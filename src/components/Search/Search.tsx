2d522a1db743df5b98bd950e12630cf4beec2e4c
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-05-24 15:37:18 +0300

    refactor: search to form

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,16 +1,25 @@
 import { Button, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import SearchIcon from "@/assets/svg/searchIcon";
 import searchStyle from "./search.module.scss";
 
 export default function Search() {
+  const form = useForm({
+    initialValues: {
+      search: "",
+    },
+  });
   return (
-    <div className={searchStyle.search}>
+    <form className={searchStyle.search} onSubmit={(value) => {}}>
       <TextInput
         placeholder="Введите название вакансии"
         icon={<SearchIcon />}
         className={searchStyle.search__input}
+        {...form.getInputProps("search")}
       />
-      <Button className={searchStyle.search__btn}>Поиск</Button>
-    </div>
+      <Button className={searchStyle.search__btn} type="submit">
+        Поиск
+      </Button>
+    </form>
   );
 }
